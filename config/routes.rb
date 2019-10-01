e21410008cc48650528a3d089ee2a47119759a18
Antonio Ricardo <antonioricardoarfs@gmail.com>
2019-10-01 09:51:12 -0400

    Creating controller to Videos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'videos/index'
+  get 'videos/show'
+  get 'videos/new'
+  get 'videos/edit'
+  get 'videos/create'
+  get 'videos/update'
+  get 'videos/destroy'
   root to: 'home#index'
   get 'home/index'
   devise_for :users, :controllers => { registrations: 'registrations' }
