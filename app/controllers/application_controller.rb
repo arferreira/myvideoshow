a2ed6c130c2a8d31d22a2dd0589e70e72ce4d1ee
Antonio Ricardo <antonioricardoarfs@gmail.com>
2019-10-01 11:51:48 -0400

    Redirect user to root app when 404 error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+    rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+    # redirect not found to root 
+    def render_404
+    redirect_to main_app.root_url
+    end
 end
